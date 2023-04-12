c769411253823919c05d5e8d42b20fb4427a1f39
huy1741 <huy1741@gmail.com>
2023-04-12 22:04:13 +0300

    Added action in controller

--- src/comicweb/Controllers/ComicBooksController.cs
+++ src/comicweb/Controllers/ComicBooksController.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace comicweb.Controllers
 {
-    public class ComicBooksController : Controllers
+    public class ComicBooksController : Controller
     {
+        public string Detail()
+        {
+            return "Hello from the comicbook controller.";
+        }
     }
 }
